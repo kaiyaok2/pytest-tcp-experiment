e63d4571c50142fb843ccaa17861c0bf5171cd33
Kaiyao Ke <kaiyaok2@illinois.edu>
2024-02-02 05:25:46 +0000

    updated new logs

--- falcon-results/run_script/run_50_commits.sh
+++ falcon-results/run_script/run_50_commits.sh
@@ -5,7 +5,7 @@ while read -u 3 line; do
 	cur=$(echo $line | cut -d ' ' -f 1)
 	echo ====working on commit $cur======
 	git checkout -f $cur
-	pip install -r requirements.txt
+	pip install .
 	#cp ../../conftest.py ./
 	python3 -m pytest -s > ${cur}_no_tcp.log
         grep "current test FAILED" ${cur}_no_tcp.log | cut -d , -f 1 | cut -d ' ' -f 5,6,7,8 | rev | cut -c 2- | rev > ${cur}_no_tcp_result.log
@@ -15,7 +15,7 @@ while read -u 4 line; do
         cur=$(echo $line | cut -d ' ' -f 1)
         echo ====working on commit $cur======
         git checkout -f $cur
-        pip install -r requirements.txt
+        pip install .
         #cp ../../conftest.py ./
         python3 -m pytest --tcp --tcp-weight=1-0 -s > ${cur}_tcp_1-0.log
         grep "current test FAILED" ${cur}_tcp_1-0.log | cut -d , -f 1 | cut -d ' ' -f 5,6,7,8 | rev | cut -c 2- | rev > ${cur}_tcp_1-0_result.log
@@ -25,7 +25,7 @@ while read -u 5 line; do
         cur=$(echo $line | cut -d ' ' -f 1)
         echo ====working on commit $cur======
         git checkout -f $cur
-        pip install -r requirements.txt
+        pip install .
         #cp ../../conftest.py ./
         python3 -m pytest --tcp --tcp-weight=0-1 -s > ${cur}_tcp_0-1.log
         grep "current test FAILED" ${cur}_tcp_0-1.log | cut -d , -f 1 | cut -d ' ' -f 5,6,7,8 | rev | cut -c 2- | rev > ${cur}_tcp_0-1_result.log
@@ -35,7 +35,7 @@ while read -u 6 line; do
         cur=$(echo $line | cut -d ' ' -f 1)
         echo ====working on commit $cur======
         git checkout -f $cur
-        pip install -r requirements.txt
+        pip install .
         python3 -m pytest --tcp --tcp-weight=0.5-0.5 -s > ${cur}_tcp_0.5-0.5.log
         grep "current test FAILED" ${cur}_tcp_0.5-0.5.log | cut -d , -f 1 | cut -d ' ' -f 5,6,7,8 | rev | cut -c 2- | rev > ${cur}_tcp_0.5-0.5_result.log
 done 6< last_50_commits
